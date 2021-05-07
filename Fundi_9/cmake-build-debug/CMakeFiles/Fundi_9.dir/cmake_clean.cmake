53fe53edfa49efe0c7a885835e2c7d451a7c124b
Ayttekao <60965338+Ayttekao@users.noreply.github.com>
2021-05-08 01:17:10 +0300

    Add files via upload

---
+++ Fundi_9/cmake-build-debug/CMakeFiles/Fundi_9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fundi_9.dir/main.cpp.o"
+  "Fundi_9"
+  "Fundi_9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fundi_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
